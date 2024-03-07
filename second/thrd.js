682fdb56b5528f7f5d831ffe451287451e7f92cd
jenil gajera <gajerajenil08@gmail.com>
2024-03-07 22:54:12 +0530

    function complated

---
+++ second/thrd.js
@@ -0,0 +1,21 @@
+//function
+
+//this is a function
+function jenil(p1, p2) {
+    return p1 + p2
+}
+
+//this is col prosess
+let a = jenil(2, 4)
+console.log(a)
+
+let b = jenil(20, 125)
+console.log(b)
+
+
+function Addition(p1, p2, p3, p4, p5) {
+    return p1 + p2 + p3 - p4 + p5
+}
+
+let c = Addition(10, 20, 50, 40, 60)
+console.log(c)
