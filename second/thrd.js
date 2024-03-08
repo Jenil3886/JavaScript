dfa65f8bc3b349214374a4e51cc948294a74799e
jenil gajera <gajerajenil08@gmail.com>
2024-03-08 09:06:57 +0530

    modification complate

--- second/thrd.js
+++ second/thrd.js
@@ -6,6 +6,8 @@ function jenil(p1, p2) {
 }
 
 //this is col prosess
+
+//Ex1
 let a = jenil(2, 4)
 console.log(a)
 
@@ -13,9 +15,20 @@ let b = jenil(20, 125)
 console.log(b)
 
 
+//Ex2
+
 function Addition(p1, p2, p3, p4, p5) {
     return p1 + p2 + p3 - p4 + p5
 }
 
 let c = Addition(10, 20, 50, 40, 60)
 console.log(c)
+
+//Ex3
+
+function alltypes(p1, p2, p3, p4, p5, p6, p7, p8, p9) {
+    return p1 + p2 - p3 * p4 / p5 % p6 - p7 + p8 * p9
+}
+
+let d = alltypes(10, 15, 14, 18, 30, 25, 65, 85, 41)
+console.log(d)
