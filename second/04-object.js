32f3b93f99cb93543fc496695f9e14d7c4372f0b
jenil gajera <gajerajenil08@gmail.com>
2024-03-12 11:32:16 +0530

    object

--- second/04-object.js
+++ second/04-object.js
@@ -4,9 +4,14 @@
 // object literals
 
 // this is object
+
+const mysym = Symbol("key1")
+
 const jsuser = {
     name: "jenil",
     "full name": "jenilgajera",
+    mysym: "mykey1",//this is symbol but is not wark in symbol 
+    [mysym]: "mykey1",
     age: 18,
     location: "surat",
     email: "jenilgajera08@gmail.com",
@@ -23,7 +28,9 @@ console.log(jsuser.email)
 console.log(jsuser["email"])
 
 console.log(jsuser["full name"])
-
+console.log(jsuser["mysym"])
+//this is right type of symbol run
+console.log(jsuser[mysym])
 
 
 
