ba0862d60ef1e223ac53d42926e62b9574a099f2
jenil gajera <gajerajenil08@gmail.com>
2024-03-13 10:35:14 +0530

    string add

--- second/04-object.js
+++ second/04-object.js
@@ -88,14 +88,3 @@ const student2 = {
 console.log(student2.rollNo)
 console.log(student2['rollNo'])
 
-const variable = "gajera"
-
-const a = "jenil variable" // double quotes
-const b = 'jenil variable' // single quote, same as above
-const c = `jenil ${variable}` // we can use dynamic variables
-
-console.log(a)
-console.log(b)
-console.log(c)
-
-
---
+++ second/05_string.js
@@ -0,0 +1,13 @@
+// -------------------string----------------------------//
+
+const variable = "gajera"
+
+const a = "jenil variable" // double quotes
+const b = 'jenil variable' // single quote, same as above
+const c = `jenil ${variable}` // we can use dynamic variables
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+
