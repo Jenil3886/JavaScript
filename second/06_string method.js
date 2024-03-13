f32b2a330d77bd3e99dde0fc0fa3cfbe424d97a4
jenil gajera <gajerajenil08@gmail.com>
2024-03-13 15:55:04 +0530

    string slice complait

---
+++ second/06_string method.js
@@ -0,0 +1,13 @@
+
+//----------------------string.slice()----------------------------//
+
+// The method takes 2 parameters: start position, and end position (end not included).
+let fruit = "maylo, Jayraj, umo"
+let riya = fruit.slice(7,13)
+
+console.log(riya)
+
+// const  jenil = 'maylo, jayraj, umo'    //
+// let rajnit = jenil.slice(7,13)         //   this is not right way in print in string.slice()
+                                          //
+// console.log(rajanit)                   //
