a5ba49d3c017b617c751405cce86a658bd621975
jenil gajera <gajerajenil08@gmail.com>
2024-03-14 10:05:23 +0530

    substring complat

--- second/06_string method.js
+++ second/06_string method.js
@@ -3,13 +3,13 @@
 
 // The method takes 2 parameters: start position, and end position (end not included).
 let fruit = "maylo, Jayraj, umo"
-let riya = fruit.slice(7,13)
+let riya = fruit.slice(7, 13)
 
 console.log(riya)
 
 // const  jenil = 'maylo, jayraj, umo'    //
 // let rajnit = jenil.slice(7,13)         //   this is not right way in print in string.slice()
-                                          //
+//
 // console.log(rajanit)                   //
 
 const text = "jenil, sanket, darshan"
@@ -29,3 +29,23 @@ const jenill = apple.slice(-12)
 
 console.log(jenill)
 
+
+//------------------------------substring() --------------------------------------------//
+
+//substring() is similar to slice().
+//The difference is that start and end values less than 0 are treated as 0 in substring().
+
+const teext = "apple, banana, kivi"
+const part = teext.substring(7, 13)
+
+console.log(part)
+
+// -------------------------------substr()---------------------------------------------//
+
+// substr() is similar to slice().
+// The difference is that the second parameter specifies the length of the extracted part.
+
+// const kivi = "apple, banana, kivi"
+// const paart = kivi.substr(paart)
+
+// console.log(paart)
