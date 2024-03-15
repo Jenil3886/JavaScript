2c293e2bb3f217ae885ad780c19bb2618ff4d1bc
jenil gajera <gajerajenil08@gmail.com>
2024-03-15 22:37:07 +0530

    concet method complate

--- second/06_string method.js
+++ second/06_string method.js
@@ -57,3 +57,4 @@ function abc () {
 abc()
 
 kivi.includes()
+
---
+++ second/07_stringmethod.js
@@ -0,0 +1,19 @@
+//----------------------------------------concat()---------------------------------------------------//
+//concat() joins two or more strings
+
+const name1 = " jenil"
+const name2 = " gajera"
+const name3 = name1.concat(" ", name2)
+
+console.log(name3)
+
+//Ex...
+
+const text1 = " Hii"
+const text2 = " I"
+const text3 = " am"
+const text4 = " a"
+const text5 = " jenil"
+const text6 = " gajera"
+
+console.log(" ",text1, text2, text3, text4, text5, text5, text6)
