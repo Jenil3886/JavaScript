4aa42e38b738dbb7035dc8946fad623437fe6886
jenil gajera <gajerajenil08@gmail.com>
2024-03-19 14:59:28 +0530

    add panEnd

--- second/08_string-method.js
+++ second/08_string-method.js
@@ -3,6 +3,16 @@
 //It pads a string with another string (multiple times) until it reaches a given length.
 
 const text = "jenil"
-const type = text.padStart(10,"0")
+const type = text.padStart(10, "0")
 
 console.log(type)
+
+//----------------------------------padEnd()----------------------------------//
+//The padEnd() method pads a string from the end.
+//It pads a string with another string (multiple times) until it reaches a given length.
+
+const jenil = "jenil"
+const jenil1 = text.padEnd(10, "0")
+
+console.log(jenil)
+
