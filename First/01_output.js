f17176ccd572a2b5a3f41b5d2193c174faed054a
jenil gajera <gajerajenil08@gmail.com>
2024-03-22 20:53:41 +0530

    restart

--- First/01_output.js
+++
@@ -1,5 +0,0 @@
-// 4 ways to display output
-
-// 1
-
-console.log('Jenil Gajera');
--- First/02_output.js
+++
@@ -1,7 +0,0 @@
-document.write('Jenil Gajera')
-
-console.log('Jenil Gajera')
-
-window.alert('Jenil Gajera')
-
-document.getElementById('header').innerHTML = 'I am Jenil Gajera'
