2172f5604996bfe61fa8cff90c6f6279208e8bd3
jenil gajera <gajerajenil08@gmail.com>
2024-03-28 19:32:59 +0530

    datatypes

---
+++ second/02_Data_types.js
@@ -0,0 +1,21 @@
+// two types of data types : primitive and non-primitive(referance) data types
+
+// primitive data types :
+
+let a = 10               // an integer
+let b = 12.45            // a flooting point    
+let c = "JavaScript"     // a JavaScript
+let d = true             // a boolean value
+let e = false            // a boolean value
+let f = null             // a null value
+let g = undefined        // an undefined value
+let h = ""               // an empty string
+let i                    // an undefined value
+
+console.log(a) // console.log() function is used to print or display anything while running the code
+
+// above all are valide data types 
+
+// non-primitive data types:
+
+// object, array, regexp
