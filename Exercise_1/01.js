ece3833db867eddadf1ecab82c5aaa210b09c7fa
Jenil Gajera <146574715+Jenil3886@users.noreply.github.com>
2024-05-29 17:22:52 +0530

    JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.

--- Exercise_1/01.js
+++ Exercise_1/01.js
@@ -9,5 +9,15 @@ if (x.getHours < 12)
     y = "AM"
 else y = "PM"
 
+// or
+
+if (x.getHours < 12){
+    y = "AM"
+}else if(x.getHours > 12){
+    y = "PM"
+}else{
+    y = "12"
+}
+
 console.log("Today is : " + myArray[x.getDay()]+ "." )
 console.log("currant time is : " + x.getHours() + " " + y + " : " + x.getMinutes() + " : " + x.getSeconds());
